41b54543ea14263e1d7ca53da1571570f6211298
Behpakhsh It Department <behpakhsh.it@gmail.com>
2020-05-14 13:54:53 +0330

    Add -> method onDetachedFromWindow()

--- customviewpager/src/main/java/bpc/dis/customviewpager/CustomViewPager.java
+++ customviewpager/src/main/java/bpc/dis/customviewpager/CustomViewPager.java
@@ -1,14 +1,17 @@
 package bpc.dis.customviewpager;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 
+import androidx.annotation.NonNull;
 import androidx.viewpager.widget.ViewPager;
 
 public class CustomViewPager extends ViewPager {
 
     private boolean enabled;
+    private OnPageChangeListener onPageChangeListener;
 
     public CustomViewPager(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -16,6 +19,7 @@ public class CustomViewPager extends ViewPager {
     }
 
     @Override
+    @SuppressLint("ClickableViewAccessibility")
     public boolean onTouchEvent(MotionEvent event) {
         if (this.enabled) {
             return super.onTouchEvent(event);
@@ -29,6 +33,20 @@ public class CustomViewPager extends ViewPager {
             return super.onInterceptTouchEvent(event);
         }
         return false;
+    }
+
+    @Override
+    public void addOnPageChangeListener(@NonNull OnPageChangeListener onPageChangeListener) {
+        super.addOnPageChangeListener(onPageChangeListener);
+        this.onPageChangeListener = onPageChangeListener;
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        if (onPageChangeListener != null) {
+            removeOnPageChangeListener(onPageChangeListener);
+        }
     }
 
     public boolean isPagingEnabled() {
